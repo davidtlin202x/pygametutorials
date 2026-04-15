43a96a2a5d9ba16e72a4c44d52f570862881c673
davidtlin202x <david.t.lin2@gmail.com>
2026-04-15 12:25:24 +0800

    Update enemy.py

--- scrollergame415/enemy.py
+++ scrollergame415/enemy.py
@@ -1,1 +1,30 @@
+import pygame
 
+ENEMY_COLOR = (200, 50, 75)
+ENEMY_SPEED = 2
+ENEMY_SIZE = 35
+
+class Enemy:
+
+    def __init__(self, platform, offset=0):
+        self.platform = platform
+        self.rect = pygame.Rect(
+            platform.rect.x + offset,
+            platform.rect.y - ENEMY_SIZE,
+            ENEMY_SIZE, ENEMY_SIZE
+        )
+        self.direction = 1
+    
+    def movement(self):
+        self.rect.x += ENEMY_SPEED * self.direction
+        if self.rect.left <= self.platform.rect.left:
+            self.direction = 1
+        elif self.rect.right >= self.platform.rect.right:
+            self.direction = -1
+    
+    def draw(self, win, camera_x):
+        draw_rect = self.rect.move(-camera_x, 0)
+        pygame.draw.rect(win, ENEMY_COLOR, draw_rect)
+    
+    def check_player_collision(self, player):
+        return self.rect.colliderect(player)
